5a566caff30dc65d802cfd112aafbbd849b49e3c
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-06 19:36:00 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ items={}
 def first_example():
     return {"message": "Hello, FastAPI!"}
 
-@app.post("/items/")
+@app.post("/items")
 async def create_item(item: Item):
     item_id=len(items)+1
     items[item_id]=item

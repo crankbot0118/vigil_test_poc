e028bf5433f76826d421db0bd2443161cadd2eb4
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-06 19:28:59 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+items={}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_id=len(items)+1
+    items[item_id]=item
+    return{"id": item_id, "item":item}
 
 @app.get("/")
 def first_example():

37fcaa6b9c3ffe688c12931fff2fe6c22e269c45
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-06 19:35:04 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,12 @@ class Item(BaseModel):
 
 items={}
 
+@app.get("/")
+def first_example():
+    return {"message": "Hello, FastAPI!"}
+
 @app.post("/items/")
 async def create_item(item: Item):
     item_id=len(items)+1
     items[item_id]=item
     return{"id": item_id, "item":item}
-
-@app.get("/")
-def first_example():
-    return {"message": "Hello, FastAPI!"}
